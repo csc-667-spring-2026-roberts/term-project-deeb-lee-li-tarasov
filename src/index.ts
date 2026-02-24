8dec96c1acfb1ccfb9295feabbbacc58a808c829
AkimT13 <akimtarasov@gmail.com>
2026-02-23 20:52:56 -0800

    changed port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-const PORT = Number(process.env.PORT ?? 3005);
+const PORT = process.env.PORT || 3005;
 
 app.get("/", (request: Request, response: Response) => {
   response.send("<h1>Express</h1>");
