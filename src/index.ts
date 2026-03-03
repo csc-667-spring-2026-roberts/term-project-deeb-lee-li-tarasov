a04b00fa366eb0eb0a4af9d9e8ff3c22fb5402dc
AkimT13 <akimtarasov@gmail.com>
2026-03-02 16:12:30 -0800

    adding images and a route for images, M4 complete

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express, { Request, Response } from "express";
-
+import path from "path";
 const app = express();
 
 const PORT = process.env.PORT || 3005;
-
-app.get("/", (request: Request, response: Response) => {
-  response.send("<h1>Express</h1>");
+app.use(express.static(path.join(process.cwd(), "public")));
+app.get("/", (_req: Request, res: Response) => {
+  res.send("<h1>Header</h1>");
 });
 
+app.get("/image/:id", (req: Request, res: Response) => {
+  const imageId = req.params.id as string;
+
+  const filePath = path.join(process.cwd(), "public", `${imageId}.jpg`);
+  try {
+    res.sendFile(filePath);
+  } catch {
+    res.status(400).send("Image does not exist");
+  }
+});
 app.listen(PORT, () => {
-  console.log(`Server running on`);
+  console.log(`Test hot reload by changing this line`);
 });
